999d3995ae390c30c410cb2d60d28fcbc71453d5
juan-carlos-mamani-gutierrez <jiseroca65528@gmail.com>
2023-10-31 00:07:09 -0400

    feat:

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -2,12 +2,17 @@
 
 namespace app\Controllers;
 
+use app\Models\Contact;
+
 
 class HomeController extends Controller
 {
 
     public function index()
     {
+
+        $contacModel = new Contact();
+
         return $this->view('home',[
            'title' => 'Home',
            'description' => 'esta es la pagina home'
--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once '../config/database.php';
+
 require_once '../autoload.php';
 /* require_once '../lib/Route.php'; */
 require_once '../routes/web.php';
